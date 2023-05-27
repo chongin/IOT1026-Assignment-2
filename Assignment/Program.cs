f1fe313722d2c0f9bed26dfbf92d85701be444df
ngchongin <ngchongin@gmail.com>
2023-05-26 21:15:20 -0400

    invoke TreasureChest class function in the main function

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Assignment
+﻿
+namespace Assignment
 {
     static class Program
     {
         static void Main()
         {
             Console.WriteLine("Hello, World!");
+            TreasureChest chest = new TreasureChest();
+            var state = chest.Manipulate(TreasureChest.Action.Unlock);
+            Console.WriteLine($"Unlock it success, and return state:{state}");
+            Console.WriteLine(chest.ToString());
         }
     }
 }
